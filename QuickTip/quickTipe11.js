95b43e26c11b1af8e0962b1ef159d57f54b99db8
paozanwa22 <fausanwadi@gmail.com>
2020-03-28 18:14:24 +0800

    menambahkan No Rekening

---
+++ QuickTip/quickTipe11.js
@@ -0,0 +1,63 @@
+{
+	// Place your snippets for html here. Each snippet is defined under a snippet name and has a prefix, body and 
+	// description. The prefix is what is used to trigger the snippet and the body will be expanded and inserted. Possible variables are:
+	// $1, $2 for tab stops, $0 for the final cursor position, and ${1:label}, ${2:another} for placeholders. Placeholders with the 
+	// same ids are connected.
+	// Example:
+	// "Print to console": {
+	// 	"prefix": "log",
+	// 	"body": [
+	// 		"console.log('$1');",
+	// 		"$2"
+	// 	],
+	// 	"description": "Log output to console"
+    // }
+
+
+    
+    // my snippet yang di tulis di file html.json   penjelasan di chenel web perograming UNPAS
+	// "PHP Tag": {
+	// 	"prefix": "php",
+	// 	"body": "<?php $1 ?>"
+	// },
+
+	// "Inline Echo": {
+	// 	"prefix": "phpp",
+	// 	"body": "<?= $$1; ?>"
+	// },
+
+	// "Input Labes": {
+	// 	"prefix": "formel",
+	// 	"body": [
+	// 		"<label for=\"${2:username}\">${3:Username}</label>",
+	// 		"<input type=\"${1:text}\" name=\"${2:username}\" id=\"${2:username}\">"
+	// 	]
+	// },
+
+	// "my Form": {
+	// 	"prefix": "myform",
+	// 	"body": "<form method=\"${1|get,post|}\" action=\"$2\">$3</form>"
+	// },
+
+	// "awal php": {
+	// 	"prefix": "ps",
+	// 	"body": [
+	// 		"<?php ",
+	// 			"\t$1",
+	// 		"?>"
+	// 	]	
+    // }
+    
+
+    // my snippet yang di tulis di file new global snippets file..   penjelasan di chenel web perograming UNPAS QuickTip#11
+    // "Rekening BRI": {
+	// 	"scope": "javascript,php,c++,html,css",
+	// 	"prefix": "rekening",
+	// 	"body": "470201023951539	Paozan Wadi"
+    // }
+    
+
+    // Untuk melihat new snippets global di simpan
+    // C:\Users\StankyNoob\AppData\Roaming\Code\User\snippets   // unhidden file untuk masuk ke AppData.
+
+}
