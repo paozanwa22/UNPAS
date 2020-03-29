18fe8ffe1adfc25d734b9a4a70689713eb455a74
paozanwa22 <61973920+paozanwa22@users.noreply.github.com>
2020-03-29 22:38:26 +0800

    pertemuan2
    Catatan di video ke 2

---
+++ phpdasar/pertemuan2/index.php
@@ -0,0 +1,42 @@
+<?php 
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print  //untuk mencetak tulisan atau variable
+// print_r()     //khusus utuk mencetak Array
+// var_dump     //untuk melihat variable dan biasanya digunakan untuk mencari bag
+
+// penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+    // tidak boleh dawali dengan angka, tapi boleh mengandung angka
+
+
+// Operator
+// aritmatika
+// + - * / %
+
+// penggabung string / concatenation / concat
+// .
+
+
+// Assignment
+// =, +=, -=, *=, /=, %=, .=
+
+// perbandingan (hanya mengecek nilai nya saja)
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// identitas    (menecek nilai dan tipe datanya)
+// ===, !==
+// var_dump(1 === "1");
+
+// Logika   (biasa di pakai pada pengkondisian)
+// &&, ||, !
+
+
+?>
