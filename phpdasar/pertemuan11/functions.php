8e95b5807b22beed5d6c4e531960a561aec40f98
paozanwa22 <fausanwadi@gmail.com>
2020-04-02 15:16:52 +0800

    pertemuan 11 menambahkan fungsi ubah

---
+++ phpdasar/pertemuan11/functions.php
@@ -0,0 +1,57 @@
+<?php 
+// koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+function query($query){
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while( $row = mysqli_fetch_assoc($result) ){
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+function tambah($data){
+    global $conn;
+    $gambar = htmlspecialchars($data["gambar"]);
+    $nama = htmlspecialchars($data["nama"]);
+    $nrp = htmlspecialchars($data["nrp"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+
+    $query = "INSERT INTO mahasiswa VALUE
+                ('','$nama','$nrp','$email','$gambar','$jurusan')
+            ";
+    mysqli_query($conn, $query);
+    return mysqli_affected_rows($conn);
+}
+
+function hapus( $id ){
+    global $conn;
+    mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id");
+    return mysqli_affected_rows($conn);
+}
+
+function ubah($data){
+    global $conn;
+    $id = $_GET["id"];
+    $gambar = htmlspecialchars($data["gambar"]);
+    $nama = htmlspecialchars($data["nama"]);
+    $nrp = htmlspecialchars($data["nrp"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+
+    $query = "UPDATE mahasiswa SET
+                nama = '$nama',
+                nrp = '$nrp',
+                email = '$email',
+                gambar = '$gambar',
+                jurusan = '$jurusan'
+            WHERE id = $id
+            ";
+    mysqli_query($conn, $query);
+    return mysqli_affected_rows($conn);
+}
+
+?>
